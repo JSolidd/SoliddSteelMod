a6cba2f06a9b2502d8dbc88925aa89de5a8139fa
JSolidd <jacksoliday@outlook.com>
2022-02-01 21:35:59 -0800

    Advancements + packed ore!

--- src/main/java/com/jsolidd/soliddmod/block/ModBlocks.java
+++ src/main/java/com/jsolidd/soliddmod/block/ModBlocks.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,16 +23,20 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, SoliddMod.MOD_ID);
 
     public static final RegistryObject<Block> SOLIDD_STEEL_BLOCK = registerBlock("solidd_steel_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL)
+                    .strength(5f, 7f).requiresCorrectToolForDrops().sound(SoundType.METAL)), ModCreativeModeTab.SOLIDD_TAB);
+
+    public static final RegistryObject<Block> PACKED_SOLIDD_ORE = registerBlock("packed_solidd_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f, 6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
 
     public static final RegistryObject<Block> SOLIDD_ORE_BLOCK = registerBlock("solidd_ore_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
+                    .strength(3f, 3f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
 
     public static final RegistryObject<Block> DEEPSLATE_SOLIDD_ORE = registerBlock("deepslate_solidd_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.DEEPSLATE)
-                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).sound(SoundType.DEEPSLATE)
+                    .strength(4.5f, 3f).requiresCorrectToolForDrops()), ModCreativeModeTab.SOLIDD_TAB);
 
 
     //Helpers
